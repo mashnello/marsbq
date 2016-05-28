cd81faa204d7233d2fd822965b0ad1b5657849d3
mashnello <mashnenko@mail.ru>
2016-05-28 20:09:27 +0300

    added handlebars templates

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,17 @@ var minifyCss = require('gulp-minify-css');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var imageMin = require('gulp-imagemin');
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
+
+gulp.task('templates', function() {
+	return gulp.src(['src/templates/**/*.hbs'])
+		.pipe(handlebars())
+		.pipe(rename(function(path) {
+			path.extname = '.html'
+		}))
+		.pipe(gulp.dest('./'));
+});
 
 gulp.task('images', function() {
 	gulp.src('src/img/**/*')
@@ -30,12 +41,13 @@ gulp.task('styles', function() {
 		.pipe(browserSync.stream());
 });
 
-gulp.task('default', function() {
+gulp.task('default', ['styles', 'scripts', 'images', 'templates'], function() {
 	browserSync.init({
 		server: './'
 	});
 	gulp.watch('src/styles/**/*.css', ['styles']);
 	gulp.watch('src/scripts/**/*.js', ['scripts']);
 	gulp.watch('src/img/**/*', ['images']);
+	gulp.watch('src/templates/**/*.hbs', ['templates']);
 	gulp.watch('*.html', browserSync.reload);
 });
